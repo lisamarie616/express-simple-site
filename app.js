94db9763e9816cc178b8b47f2abb691a1bfa570e
lisamarie616 <lisamarie616@gmail.com>
2016-08-01 23:02:15 -0700

    add nodemailer contact form

--- app.js
+++ app.js
@@ -13,11 +13,43 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  res.render('index',{title: 'Welcome'});
+  res.render('index',{title: 'Computer Not Working?'});
 });
 
 app.get('/about', function(req, res){
   res.render('about');
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact');
+});
+
+app.post('/contact/send', function(req, res){
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      //user: [EMAIL_HERE],
+      //pass: [PASSWORD_HERE]
+    }
+  });
+
+  var mailOptions = {
+    //from: [NAME <EMAIL>],
+    //to: [EMAIL],
+    subject: 'Website Submission',
+    text: 'You have a submission with the following details... Name: ' + req.body.name + ' Email: ' + req.body.email + ' Message: ' + req.body.message,
+    html: '<p>You have a submission with the following details...</p><ul><li>Name: ' + req.body.name + '</li><li> Email: ' + req.body.email + '</li><li>Message: ' + req.body.message + '</li></ul>'
+  };
+
+  transporter.sendMail(mailOptions, function(err, info){
+    if(err){
+      console.log(err);
+      res.redirect('/');
+    } else {
+      console.log('Message Sent: ' + info.response);
+      res.redirect('/');
+    }
+  })
 });
 
 app.listen(3000);
